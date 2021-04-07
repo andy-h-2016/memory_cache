fac9b3b6c19a73aaa2cb2eb742ecc3c9d73a1a84
Andy Huang <andy.k.huang@gmail.com>
2021-04-06 19:37:10 -0700

    add first name and last name to users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(username: 'hagrid', email: "bigbro@pottermore.com", password: 'nevermore')
+User.create(username: 'hagrid', email: "bigbro@pottermore.com", password: 'nevermore', first_name: "Rubeus", last_name: "Hagrid")
