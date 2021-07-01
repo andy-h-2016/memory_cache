ae52fd3cb1ad87b1c05ec3b042304dfbe0a31f8b
Andy Huang <andy.k.huang@gmail.com>
2021-07-01 01:12:13 -0700

    Add NotesReducer to EntitiesReducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import UsersReducer from './users_reducer';
 import ListsReducer from './lists_reducer';
 import TasksReducer from './tasks_reducer';
+import NotesReducer from './notes_reducer';
 
 export default combineReducers({
   users: UsersReducer,
   lists: ListsReducer,
-  tasks: TasksReducer
+  tasks: TasksReducer,
+  notes: NotesReducer
 });
